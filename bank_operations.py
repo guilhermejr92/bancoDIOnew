89b92f8f5278667fcbe3be5029d7512686b50e51
guilhermejr92 <guizao_167@hotmail.com>
2024-04-18 14:50:30 -0300

    first commit

---
+++ bank_operations.py
@@ -0,0 +1,65 @@
+# bank_operations.py
+
+import datetime
+
+
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo += valor
+        data_hora = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        extrato += f"{data_hora}\tDepósito:\tR$ {valor:.2f}\n"
+        print("\n=== Depósito realizado com sucesso! ===")
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+
+    return saldo, extrato
+
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+
+    elif excedeu_limite:
+        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+
+    elif excedeu_saques:
+        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+
+    elif valor > 0:
+        saldo -= valor
+        data_hora = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        extrato += f"{data_hora}\tSaque:\t\tR$ {valor:.2f}\n"
+        numero_saques += 1
+        print("\n=== Saque realizado com sucesso! ===")
+
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+
+    return saldo, extrato
+
+
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n================ EXTRATO ================")
+
+    if not extrato:
+        print("Não foram realizadas movimentações.")
+    else:
+        print("{:<20} {:<15} {:<10}".format(
+            "Data/Hora", "Movimentação", "Valor"))
+        print("-" * 45)
+
+        saldo_atual = 0
+        for movimentacao in extrato.split("\n"):
+            if movimentacao:
+                data_hora, tipo_movimentacao, valor_str = movimentacao.split(
+                    "\t")
+                valor = float(valor_str.replace("R$ ", ""))
+                saldo_atual += valor
+                print("{:<20} {:<15} {:<10}".format(
+                    data_hora, tipo_movimentacao, valor_str))
+
+    print("=" * 45)
